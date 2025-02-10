564db523ff22513c4e7fcd3b6868dfd11ed7611a
Sophia <461932@bsd48.org>
2025-02-10 12:38:09 -0800

    add test file, implement display, stop overwriting when adding more nums

--- main.cpp
+++ main.cpp
@@ -7,11 +7,10 @@ using namespace std;
 void addNum (int heap[101], int &heapSize, int x);
 void add (int heap[101], int &heapSize, bool &added);
 void swap (int heap[101], int i_x, int i_y);
-void remove (int heap[101], int &heapSize, bool added);
-void removeAll (int heap[101], int &heapSize, bool added);
-void print (int heap[101], string connection, int i);
-void display (int heap[101], int heapSize, bool added);
-void printCmds (bool added);
+void remove (int heap[101], int &heapSize, bool &added);
+void removeAll (int heap[101], int &heapSize, bool &added);
+void display (int heap[101], int i, int indent, bool added);
+void printCmds ();
 
 int main () {
   int heap [101], heapSize = 0;
@@ -24,14 +23,16 @@ int main () {
 
   // main loop
   while (isRunning) {
-    printCmds (added);
+    printCmds ();
     getline (cin, cmd);
+    cout << endl;
     if (cmd == "ADD") { add (heap, heapSize, added); }
     else if (cmd == "DELETE") { remove (heap, heapSize, added); }
     else if (cmd == "DELETEALL") { removeAll (heap, heapSize, added); }
-    else if (cmd == "DISPLAY") { display (heap, heapSize, added); }
-    else if (cmd == "QUIT") { isRunning = false; }
-    else { cout << "Huh?" << endl; }    
+    else if (cmd == "DISPLAY") { display (heap, 1, 0, added); }
+    else if (cmd == "QUIT") { isRunning = false; cout << "Sorry to see you go.";}
+    else { cout << "Huh?" << endl; }
+    cout << endl;
   }
 
   return 0;
@@ -59,53 +60,63 @@ void addNum (int heap[101], int &heapSize, int x) {
 // add: main add function
 void add (int heap[101], int &heapSize, bool &added) {
   cout << "Add numbers from file (F) or standard input/output (C)?" << endl;
-  char input; cin >> input;
+  char input; cin >> input; cin.get();
+  int h = heapSize; // initial heap size
 
   // file input
   if (input == 'F') {
-    added = true;
     string fname;
     cout << "Name of file? ";
     getline (cin, fname);
-    cout << endl;
     ifstream fin (fname);
     int x;
-    cout << "Reading..." << endl;
+    cout << "Reading... ";
     while (fin >> x) {
       addNum (heap, heapSize, x);
     }
     fin.close();
-    cout << "Done." << endl;
+    if (heap[h + 1] != 0) {
+      added = true;
+      cout << endl << "Done." << endl;
+    }
+    else {
+      cout << endl << "Error reading file." << endl;
+    }
   }
   
   // standard input
   else if (input == 'C') {
     int n, x;
     cout << "How many numbers? ";
-    cin >> n;
+    cin >> n; cin.get();
     cout << endl;
-    cout << "Reading..." << endl;
-    for (int i = 0; i < n; i++) {
+    cout << "Reading... ";
+    for (int i = h; i < h + n; i++) {
       cin >> x;
       addNum (heap, heapSize, x);
     }
     cin.get();
-    cout << "Done." << endl;    
+    added = true;
+    cout << endl << "Done." << endl;    
   }
 
   // bad input
   else { cout << "I don't know what you mean." << endl; }
+
   return;
 }
 
 // 2: DELETE
-void remove (int heap[101], int &heapSize, bool added) {
+void remove (int heap[101], int &heapSize, bool &added) {
+  if (!added || heap[1] == 0) return;
+  
   // output root
   cout << heap[1] << endl;
 
   // move bottom-most element to the top and remove the original root
   heap[1] = 0;
   swap(heap, heapSize, 1);
+  heapSize --;
 
   // move down the tree
   int i = 1;
@@ -120,22 +131,31 @@ void remove (int heap[101], int &heapSize, bool added) {
       i *= 2; i++;
     }
   }
+
+  if (heap[1] == 0) { added = false; }
+
   return;
 }
 
 // 3: DELETE ALL
-void removeAll (int heap[101], int &heapSize, bool added) {
-  if (!added) return;
+void removeAll (int heap[101], int &heapSize, bool &added) {
+  if (!added || heap[1] == 0) return;
   while (heap[1] != 0) {
     remove (heap, heapSize, added);
   }
+  added = false;
   return;
 }
 
 // 4: DISPLAY
 // display: nicely prints the tree
-void display (int heap[101], int heapSize, bool added) {
-  
+void display (int heap[101], int i, int indent, bool added) {
+  if (!added || heap[i] == 0) return;
+  display(heap, i * 2 + 1, indent + 1, added);
+  for (int i = 0; i < indent; i++) { cout << "    "; }
+  cout << heap[i] << endl;
+  display(heap, i * 2, indent + 1, added);
+  return;
 }
 
 // HELPER FUNCTIONS
@@ -149,9 +169,9 @@ void swap (int heap[101], int i_x, int i_y) {
 }
 
 // printCmds: prints available commands for the user
-void printCmds (bool added) {
+void printCmds () {
   cout << "Your commands:" << endl;
-  if (add) cout << "- ADD -- add numbers to the heap" << endl;
+  cout << "- ADD -- add numbers to the heap" << endl;
   cout << "- DELETE -- delete the maximum of the heap" << endl;
   cout << "- DELETEALL -- delete everything in the heap" << endl;
   cout << "- DISPLAY -- display the heap" << endl;
